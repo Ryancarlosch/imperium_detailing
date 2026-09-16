72067d0336b20429337657b7467bd88f9d206ca9
Ryancarlosch <ryan.carlos148@gmail.com>
2026-09-15 23:25:18 -0300

    chore(ci): valida onboarding formatado no web

--- lib/main_web_bootstrap.dart
+++ lib/main_web_bootstrap.dart
@@ -2,6 +2,7 @@ import 'package:flutter_web_plugins/url_strategy.dart';
 
 import 'main_web.dart' as imperium_web;
 
+// O Web usa o mesmo fluxo de autenticação e empresa do aplicativo móvel.
 Future<void> main() async {
   usePathUrlStrategy();
   await imperium_web.main();
