1e6fda99b6fb2f5ac16e242f32ea0d7ed5d61191
Ryancarlosch <ryan.carlos148@gmail.com>
2026-09-18 10:06:16 -0300

    test(sync): protege reconciliacao pre-upload operacional

---
+++ test/operacional_sync_pre_reconciliacao_source_test.dart
@@ -0,0 +1,48 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('operacional reconcilia antes de publicar e bloqueia conflitos', () {
+    final source = File(
+      'lib/services/operacional_sync_service.dart',
+    ).readAsStringSync();
+
+    final inicio = source.indexOf(
+      'Future<void> _syncOperacionalBase(String empresaId) async',
+    );
+    final fim = source.indexOf(
+      'Future<void> _syncOrdensServico(String empresaId) async',
+      inicio,
+    );
+
+    expect(inicio, greaterThanOrEqualTo(0));
+    expect(fim, greaterThan(inicio));
+
+    final bloco = source.substring(inicio, fim);
+
+    final reconciliarPrimeiro = bloco.indexOf(
+      'OperacionalCloudV2Service.instance.prepararUpload',
+    );
+    final publicarCliente = bloco.indexOf('_publicarClientesLocais');
+    final baixarClientes = bloco.indexOf('_baixarClientes');
+
+    expect(reconciliarPrimeiro, greaterThanOrEqualTo(0));
+    expect(publicarCliente, greaterThan(reconciliarPrimeiro));
+    expect(baixarClientes, greaterThan(publicarCliente));
+
+    expect(
+      RegExp(
+        r'OperacionalCloudV2Service\.instance\.prepararUpload',
+      ).allMatches(bloco).length,
+      greaterThanOrEqualTo(2),
+    );
+    expect(
+      bloco,
+      contains(
+        "throw const SyncMotorBloqueadoException(\n"
+        "        'Conflitos pendentes em Clientes/Veículos/Agenda.',",
+      ),
+    );
+  });
+}
