3b7b6902690bfccdb953402b2a1f7a45d75f6931
Ryancarlosch <ryan.carlos148@gmail.com>
2026-09-19 11:32:58 -0300

    fix(web): ajusta callbacks ao lint atual

--- lib/web/web_pos_venda_page.dart
+++ lib/web/web_pos_venda_page.dart
@@ -290,7 +290,7 @@ class _WebPosVendaPageState extends State<WebPosVendaPage> {
 
               return ListView.separated(
                 itemCount: itens.length,
-                separatorBuilder: (_, __) => const Divider(height: 1),
+                separatorBuilder: (_, _) => const Divider(height: 1),
                 itemBuilder: (context, index) {
                   final item = itens[index];
                   final data = _parseData(item['data_interacao']);
