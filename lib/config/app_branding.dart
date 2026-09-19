a7937639f2dbb0d7b001f1af2d488a91f13993fc
Ryancarlosch <ryan.carlos148@gmail.com>
2026-09-19 11:58:39 -0300

    refactor(brand): centraliza identidade do login

--- lib/config/app_branding.dart
+++ lib/config/app_branding.dart
@@ -10,6 +10,15 @@ class AppBranding {
   static const String developmentName = 'Imperium';
   static const String productName = 'Imperium Manager';
   static const String webTitle = 'Imperium Manager Web';
+  static const String wordmarkPrimary = 'IMPERIUM';
+  static const String wordmarkSecondary = 'MANAGER';
+
+  static const Color loginBackground = Color(0xFF07090C);
+  static const Color loginSurface = Color(0xFF101419);
+  static const Color loginSurfaceRaised = Color(0xFF171C22);
+  static const Color loginBorder = Color(0xFF2A313A);
+  static const Color loginText = Color(0xFFF5F6F8);
+  static const Color loginMuted = Color(0xFF9CA5AF);
 
   static const Color webBackground = Color(0xFF090B0E);
   static const Color webSurface = Color(0xFF11151A);
